3e7c91a18a33e3ff9c2d725af27e47ecbee045e4
khuseifah Nazneen <khuseifahnazneen@gmail.com>
2021-12-24 21:14:30 +0500

    shapes

--- app/src/main/java/com/naaz/assignmnt3/MainActivity.java
+++ app/src/main/java/com/naaz/assignmnt3/MainActivity.java
@@ -1,8 +1,11 @@
 package com.naaz.assignmnt3;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button Circle,Square,Rectangle,Triangle;
+        Square=findViewById(R.id.Square);
+        Rectangle=findViewById(R.id.Rectangle);
+        Triangle=findViewById(R.id.Triangle);
+        Circle=findViewById(R.id.Circle);
+        Square.setOnClickListener(v -> {
+            Intent intent=new Intent(getApplicationContext(),square.class);
+            startActivity(intent);
+        });
+        Rectangle.setOnClickListener(v -> {
+            Intent intent=new Intent(getApplicationContext(),rectangle.class);
+            startActivity(intent);
+        });
+        Triangle.setOnClickListener(v -> {
+            Intent intent=new Intent(getApplicationContext(),triangle.class);
+            startActivity(intent);
+        });
+        Circle.setOnClickListener(v -> {
+            Intent intent=new Intent(getApplicationContext(),circle.class);
+            startActivity(intent);
+        });
     }
 }
